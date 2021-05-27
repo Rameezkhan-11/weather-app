f56a5e9bb0663f24c44b25659c0244952427ecc7
Rameezkhan-11 <rameezkhansadozai@gmail.com>
2021-05-28 01:40:43 +0500

    Completed Weather app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React, { useState,useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import Context from './Context';
+
 import Weather from './Weather';
 function App() {
-const [weather, setWeather] = useState([{}]);
-const [city , setCity] = useState("karachi")
-
-useEffect(() => {
-  const api = async ()=>{
-    const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=92cd3f11a062a987973cf8c8cd53329e`) 
-    const dataJson = await data.json();
-    
-    
-  
-    
-    setWeather(dataJson )
-  }
-  api();
-
-},[city])  
 
 return (
  <>
-<Context.Provider value={weather}>
-
-<Weather value={setCity}/>
 
 
-</Context.Provider>
+<Weather/>
+
+
+
 
 
 
--- src/Weather.js
+++ src/Weather.js
@@ -1,35 +1,62 @@
-import React, { useContext, useState } from 'react'
-import Context from './Context';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Image1 from './Images/cloud.gif'
+import Image2 from './Images/sunny.jpeg'
 
 
-const Weather = ({ value }) => {
+const Weather = () => {
 
 
-  let weatherData = useContext(Context)
-  // setState(weatherData.temp, weatherData.temp_min,weatherData.temp_max)
-
-  // console.log(weatherData[0].temp);
-  let name1 = weatherData.name
-  
-
-
-
-
-
-
-  const changeData = (e) => {
-    e.preventDefault();
-
-    const data = e.target.value;
-    // weatherData.name = data;
-
-    value(data)
-
-
+  const [weather, setWeather] = useState();
+  const [city, setCity] = useState("karachi")
+  const [status,setStatus] = useState();
+  const currDate = new Date().toLocaleDateString();
+  let currTime = new Date().toLocaleTimeString();
+  const [time,setTime] = useState(currTime)
+  const updateTime =()=>{
+    currTime = new Date().toLocaleTimeString()
+    setTime(currTime)
 
 
   }
+  setInterval(updateTime,1000)
+  useEffect(() => {
+    const api = async () => {
+      try{
+      const url =  (`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=92cd3f11a062a987973cf8c8cd53329e`)
+      const fetchUrl = await fetch(url)
+      const dataJson = await fetchUrl.json();
+       
+          
+            setWeather(dataJson.main)
+            setStatus(dataJson.weather[0])
+
+
+          
+
+
+
+    
+    
+    }catch(e){
+        console.log(e);
+      }
+      
+      
+    }
+    api();
+    
+
+  }, [city])
+
+
+
+
+// console.log(weather);
+
+
+
 
 
 
@@ -37,116 +64,47 @@ const Weather = ({ value }) => {
 
   return (
     <>
-      <nav className="navbar navbar-light bg-primary">
-        <div className="container-fluid">
-          <a href="/" className="navbar-brand">Weather App</a>
-          <form className="d-flex mx-auto">`
-            <input className="form-control me-2" type="search" placeholder="Enter City Name" onChange={changeData} aria-label="Search" />
-          </form>
+      <nav className="navbar navbar-light bg-dark">
+        <div className="container-fluid bg-dark">
+          <a href="/" className="navbar-brand mx-auto text-white bg-dark">Weather App</a>
+         
         </div>
       </nav>
-
-      <p style={{ textAlign: "center" }}>CityName: {name1}</p>
-
-      {Object.keys(weatherData).map((data) => {
-        // console.log(data.name);
-        const data1 = weatherData[data]
-        console.log(data1);
-        console.log(data);
-        const ind = Math.random()
-        // console.log(data1);
-
-
-        // console.log(data1.temp);
-        return (
-
-          <>
-            
-            <p key={ind + 1} style={{ textAlign: "center" }}>{data1.temp} </p>
-            <p key={ind - 1} style={{ textAlign: "center" }}>{data1.temp_min}</p>
-            <p key={ind} style={{ textAlign: "center" }}>{data1.temp_max}</p>
-
-          </>
-
-        )
-
-
-
-
-      }
-
-
-      )
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </>
-
-
-  )
+      <div className="card bg-dark mt-3" style={{margin:"0 auto",width:"18rem"}}>
+  
+      <form className="d-flex mx-auto bg-dark">`
+            <input className="form-control me-2" type="search" placeholder="Enter City Name" onChange={(e)=>{setCity(e.target.value)}}  aria-label="Search" />
+          </form>
+         
+  {  !weather || !status ?(<p className="text-center bg-dark">Data not Found</p>) :(
+    <>
+{status.main !== "Clouds" ? <img src={Image2} className="card-img-top" alt="Sunny"></img> :
+<img src={Image1} className="card-img-top" alt="Clouds"></img>
 
 }
-
-
+  
+  <div className="card-body bg-dark">
+  
+    <h5 className="card-title text-white bg-dark text-center mt-1">City: {city}</h5>
+    <p className="text-center bg-dark text-white">{currDate}</p>
+    <p className="text-center bg-dark text-white">{time}</p>
+    <p className="card-text text-white bg-dark text-center">
+     Temparaute: {weather.temp}&#8451;
+    </p>
+    <p className="card-text text-white bg-dark text-center">
+    Minimum-Temparaute: {weather.temp} &#8451;
+    </p><p className="card-text text-white bg-dark text-center">
+     Maximum-Temparaute: {weather.temp}&#8451;
+    </p>
+  
+  
+  
+  </div>
+  </>
+  )
+}
+</div>
+      
+</>
+  )}
 export default Weather
